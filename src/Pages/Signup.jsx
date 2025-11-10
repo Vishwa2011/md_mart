0b69f6ad946ce6e5a30497f152eb0ef2f1be455a
Roshni0511 <roshnichauhan051@gmail.com>
2025-11-10 13:18:38 +0530

    done

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Footer from './Footer'
 
-const Login = () => {
+const Signup = () => {
   return (
     <>
-    {/* <Navbar /> */}
-        {/* <!-- Breadcrumb Section Start --> */}
+ {/* <Navbar /> */}
+     {/* <!-- Breadcrumb Section Start --> */}
     <section class="breadcrumb-section pt-0">
         <div class="container-fluid-lg">
             <div class="row">
                 <div class="col-12">
                     <div class="breadcrumb-contain">
-                        <h2 class="mb-2">Log In</h2>
+                        <h2>Sign Up</h2>
                         <nav>
                             <ol class="breadcrumb mb-0">
                                 <li class="breadcrumb-item">
-                                    <a href="/">
+                                    <a href="index.html">
                                         <i class="fa-solid fa-house"></i>
                                     </a>
                                 </li>
-                                <li class="breadcrumb-item active">Log In</li>
+                                <li class="breadcrumb-item active">Sign Up</li>
                             </ol>
                         </nav>
                     </div>
@@ -30,13 +29,13 @@ const Login = () => {
     </section>
     {/* <!-- Breadcrumb Section End --> */}
 
-    {/* <!-- log in section start --> */}
-    <section class="log-in-section background-image-2 section-b-space">
+        {/* <!-- log in section start --> */}
+    <section class="log-in-section section-b-space">
         <div class="container-fluid-lg w-100">
             <div class="row">
                 <div class="col-xxl-6 col-xl-5 col-lg-6 d-lg-block d-none ms-auto">
                     <div class="image-contain">
-                        <img src="../assets/images/inner-page/log-in.png" class="img-fluid" alt="" />
+                        <img src="../assets/images/inner-page/sign-up.png" class="img-fluid" alt="" />
                     </div>
                 </div>
 
@@ -44,20 +43,26 @@ const Login = () => {
                     <div class="log-in-box">
                         <div class="log-in-title">
                             <h3>Welcome To Fastkart</h3>
-                            <h4>Log In Your Account</h4>
+                            <h4>Create New Account</h4>
                         </div>
 
                         <div class="input-box">
                             <form class="row g-4">
                                 <div class="col-12">
-                                    <div class="form-floating theme-form-floating log-in-form">
+                                    <div class="form-floating theme-form-floating">
+                                        <input type="text" class="form-control" id="fullname" placeholder="Full Name" />
+                                        <label for="fullname">Full Name</label>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-floating theme-form-floating">
                                         <input type="email" class="form-control" id="email" placeholder="Email Address" />
                                         <label for="email">Email Address</label>
                                     </div>
                                 </div>
 
                                 <div class="col-12">
-                                    <div class="form-floating theme-form-floating log-in-form">
+                                    <div class="form-floating theme-form-floating">
                                         <input type="password" class="form-control" id="password"
                                             placeholder="Password" />
                                         <label for="password">Password</label>
@@ -69,15 +74,14 @@ const Login = () => {
                                         <div class="form-check ps-0 m-0 remember-box">
                                             <input class="checkbox_animated check-box" type="checkbox"
                                                 id="flexCheckDefault" />
-                                            <label class="form-check-label" for="flexCheckDefault">Remember me</label>
+                                            <label class="form-check-label" for="flexCheckDefault">I agree with
+                                                <span>Terms</span> and <span>Privacy</span></label>
                                         </div>
-                                        <a href="/Forgot" class="forgot-password">Forgot Password?</a>
                                     </div>
                                 </div>
 
                                 <div class="col-12">
-                                    <button class="btn btn-animation w-100 justify-content-center" type="submit">Log
-                                        In</button>
+                                    <button class="btn btn-animation w-100" type="submit">Sign Up</button>
                                 </div>
                             </form>
                         </div>
@@ -89,15 +93,17 @@ const Login = () => {
                         <div class="log-in-button">
                             <ul>
                                 <li>
-                                    <a href="https://www.google.com/" class="btn google-button w-100">
+                                    <a href="https://accounts.google.com/signin/v2/identifier?flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+                                        class="btn google-button w-100">
                                         <img src="../assets/images/inner-page/google.png" class="blur-up lazyload"
-                                            alt="" /> Log In with Google
+                                            alt="" />
+                                        Sign up with Google
                                     </a>
                                 </li>
                                 <li>
                                     <a href="https://www.facebook.com/" class="btn google-button w-100">
                                         <img src="../assets/images/inner-page/facebook.png" class="blur-up lazyload"
-                                            alt="" /> Log In with Facebook
+                                            alt="" /> Sign up with Facebook
                                     </a>
                                 </li>
                             </ul>
@@ -108,18 +114,19 @@ const Login = () => {
                         </div>
 
                         <div class="sign-up-box">
-                            <h4>Don't have an account?</h4>
-                            <a href="/Signup">Sign Up</a>
+                            <h4>Already have an account?</h4>
+                            <a href="/Login">Log In</a>
                         </div>
                     </div>
                 </div>
+
+                <div class="col-xxl-7 col-xl-6 col-lg-6"></div>
             </div>
         </div>
     </section>
     {/* <!-- log in section end --> */}
-    {/* <Footer /> */}
     </>
   )
 }
 
-export default Login
+export default Signup
