e661101d0f8aaff14c58f1d434e96d47eed76c6a
Roshni0511 <roshnichauhan051@gmail.coom>
2025-11-13 17:51:28 +0530

    set

--- src/Pages/Ordertracking.jsx
+++ src/Pages/Ordertracking.jsx
@@ -158,45 +158,46 @@ const Ordertracking = () => {
             <div class="row">
                 <div class="col-12">
                     <div class="table-responsive">
-                        <table class="table order-tab-table">
-                            <thead>
-                                <tr>
-                                    <th>Description</th>
-                                    <th>Date</th>
-                                    <th>Time</th>
-                                    <th>Location</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Order Placed</td>
-                                    <td>26 Sep 2021</td>
-                                    <td>12:00 AM</td>
-                                    <td>California</td>
-                                </tr>
+                       <table class="table order-tab-table">
+    <thead>
+        <tr>
+            <th>Description</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Location</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Order Placed</td>
+            <td>26 Sep 2021</td>
+            <td>12:00 AM</td>
+            <td>Surat, India</td>
+        </tr>
 
-                                <tr>
-                                    <td>Preparing to Ship</td>
-                                    <td>03 Oct 2021</td>
-                                    <td>12:00 AM</td>
-                                    <td>Canada</td>
-                                </tr>
+        <tr>
+            <td>Preparing to Ship</td>
+            <td>03 Oct 2021</td>
+            <td>12:00 AM</td>
+            <td>Surat, India</td>
+        </tr>
 
-                                <tr>
-                                    <td>Shipped</td>
-                                    <td>04 Oct 2021</td>
-                                    <td>12:00 AM</td>
-                                    <td>America</td>
-                                </tr>
+        <tr>
+            <td>Shipped</td>
+            <td>04 Oct 2021</td>
+            <td>12:00 AM</td>
+            <td>Surat, India</td>
+        </tr>
 
-                                <tr>
-                                    <td>Delivered</td>
-                                    <td>10 Nav 2021</td>
-                                    <td>12:00 AM</td>
-                                    <td>Germany</td>
-                                </tr>
-                            </tbody>
-                        </table>
+        <tr>
+            <td>Delivered</td>
+            <td>10 Nov 2021</td>
+            <td>12:00 AM</td>
+            <td>Surat, India</td>
+        </tr>
+    </tbody>
+</table>
+
                     </div>
                 </div>
             </div>
